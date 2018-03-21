0e72eb8cc509505564c7608f32e06f4315739fdf
Simon Zhang <simonczhang@gmail.com>
2018-03-21 12:06:38 -0500

    final update of script

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 In this exercise I write a Python script to parse out all the Kennet Investment Team members from the page. Then I find the ones that are living in the Bay Area and ones living in London.
 
 Run the script in command line or terminal and the results will be printed.
+
+
+##### SideNote
+Below is the exact text instructions for the exercise I received:
+1. Build a python app that scrapes the Kennet team website (www.kennet.com) and is able to parse out the names and titles of the people who live in the Bay Area vs. team members in London.
+
+The instructions are a bit unclear as to whether to parse out all members first, or to only parse out the Bay Area and London members. I opted to parse out names, title, and location for all members, then looks for the ones in Bay Area and ones in London. The reason I did decided to grab all members first is that it could be useful to add all members to a database in the future.
+
+If the instructions intended for me to only parse out the selected locations, I would have just written a conditional statement when looking at the location and only grab the Bay Area and London members and leave the members from other locations out.
--- ss-scraper.py
+++ ss-scraper.py
@@ -16,27 +16,42 @@ uClient.close()
 #html parsing
 page_soup = soup(page_html, 'html.parser')
 #print(page_soup.prettify())
+#create container list for all members
 container_a = page_soup.findAll('div', {'class':'team section '})
 container_b = page_soup.findAll('div', {'class':'team section end'})
 containers = container_a + container_b
-container = containers[0]
 
+#loop through each person and grab the title, name, location and add to list
+name_list = []
+location_list = []
+title_list = []
+for container in containers:
+    #get title from link to inner page for each profile
+    name_link = container.h3.a['href']
+    uClient2 = uReq(name_link)
+    page_html2 = uClient2.read()
+    uClient2.close()
+    page_soup2 = soup(page_html2, 'html.parser')
+    content = page_soup2.findAll('div', {'id':'main'})
+    content = content[0]
+    title, loc = content.h2.text.split(',')
+    #get name, location for each profile
+    name = container.h3.a.img['alt']
+    location = container.h3.span.text
+    #add to lists
+    name_list.append(name)
+    location_list.append(location)
+    title_list.append(title)
+    
 #create dictionary of investment team members
 #once created, can easily be converted for
 #addition to SQL database, analysis as Pandas dataframe, etc...
-name_list = []
-location_list = []
-for container in containers:
-    name = container.h3.a.img['alt']
-    location = container.h3.span.text
-    name_list.append(name)
-    location_list.append(location)
-    
 team_members = {}
 team_members['names'] = name_list
 team_members['locations'] = location_list
+team_members['title'] = title_list
 
-#create dataframe out of dictionary
+#create dataframe out of dictionary to find bay area members, london members
 df = pd.DataFrame.from_dict(team_members)
 print('All Team Members')
 print(df)
@@ -49,6 +64,3 @@ print('\n')
 print('London Team Members')
 print(df[df['locations'] == 'London'])
 print('\n')
-
-
-
